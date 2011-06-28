a6ecedc942498c05a432f8510826c02de806c588
Thomas Johnston <trjohnston@gmail.com>
2011-06-28 10:22:30 -0700

    Added feature: terminate numerical integration at a zero crossing.
    Created a simple example showing how one might terminate integration before t =
    tfinal.

--- lsodartest2.f
+++ lsodartest2.f
@@ -10,7 +10,7 @@
       NEQ = 4
       Y(1) =  0.150         ! theta (rad)
       Y(2) =  0.310         ! phi (rad)
-      Y(3) = -0.155         ! thetadot (rad/s)
+      Y(3) = -0.150         ! thetadot (rad/s)
       Y(4) = -0.007         ! phidot (rad/s)
       T = 0.0               ! initial time point
       TOUT = 0.005          ! first time point output is desired
@@ -32,6 +32,31 @@
       LIW = 24              !    = IWORK
       JT = 2                !    = initial value
       NG = 1                !    = number of event functions
+!      DO 40 IOUT = 1,num_steps
+!  10    CALL DLSODAR(FEX,NEQ,Y,T,TOUT,ITOL,RTOL,ATOL,ITASK,ISTATE,
+!     1     IOPT,RWORK,LRW,IWORK,LIW,JDUM,JT,GEX,NG,JROOT)
+!        event = Y(2) - 2*(Y(1))
+!        WRITE(6,20)T,Y(1),Y(2),Y(3),Y(4),event
+!  20    FORMAT(' At t =',f12.8,'      Y =',4f14.9,'  event =',f14.9)
+!        IF (ISTATE .LT. 0) GO TO 80
+!        IF (ISTATE .EQ. 2) GO TO 40
+!        WRITE(6,30)JROOT(1)
+!  30    FORMAT(5X,' The above line is a root,  JROOT =',I5)
+!        ISTATE = 2
+!        GO TO 10
+!  40    TOUT = TOUT + 0.005     ! end of DO loop
+!      WRITE(6,60)IWORK(11),IWORK(12),IWORK(13),IWORK(10),
+!     1   IWORK(19),RWORK(15)
+!  60  FORMAT(/' No. steps =',I4,'  No. f-s =',I4,'  No. J-s =',I4,
+!     1   '  No. g-s =',I4/
+!     2   ' Method last used =',I2,'   Last switch was at t =',f12.9)
+!      STOP
+!  80  WRITE(6,90)ISTATE
+!  90  FORMAT(///' Error halt.. ISTATE =',I3)
+!      STOP
+!      END
+
+
       DO 40 IOUT = 1,num_steps
   10    CALL DLSODAR(FEX,NEQ,Y,T,TOUT,ITOL,RTOL,ATOL,ITASK,ISTATE,
      1     IOPT,RWORK,LRW,IWORK,LIW,JDUM,JT,GEX,NG,JROOT)
@@ -42,8 +67,15 @@
         IF (ISTATE .EQ. 2) GO TO 40
         WRITE(6,30)JROOT(1)
   30    FORMAT(5X,' The above line is a root,  JROOT =',I5)
-        ISTATE = 2
-        GO TO 10
+        ! Terminate Integration Upon Finding a Particular Root?
+        EVENT_SLOPE = Y(4) - 2*Y(3)     ! direction of zero crossing
+        IF (EVENT_SLOPE .LT. 0) THEN    ! don't stop integrating
+            ISTATE = 2
+            GO TO 10
+        ELSEIF (EVENT_SLOPE .GT. 0) THEN
+            print*,'     Zero Crossing Found. Stopping Integration.'
+            EXIT
+        END IF
   40    TOUT = TOUT + 0.005     ! end of DO loop
       WRITE(6,60)IWORK(11),IWORK(12),IWORK(13),IWORK(10),
      1   IWORK(19),RWORK(15)
@@ -55,6 +87,9 @@
   90  FORMAT(///' Error halt.. ISTATE =',I3)
       STOP
       END
+
+
+
 
       SUBROUTINE FEX (NEQ, T, Y, YDOT)
       DOUBLE PRECISION T, Y, YDOT
